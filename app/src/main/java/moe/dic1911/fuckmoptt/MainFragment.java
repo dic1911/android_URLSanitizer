2abd87d7c52fd4ac5ea1905612028d6518368578
dic1911 <dic1911@protonmail.com>
2020-09-15 16:58:50 +0800

    init

---
+++ app/src/main/java/moe/dic1911/fuckmoptt/MainFragment.java
@@ -0,0 +1,25 @@
+package moe.dic1911.fuckmoptt;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
